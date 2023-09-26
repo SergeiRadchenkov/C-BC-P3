0b82a1b4cf4318568791be1a12d48dffee20903f
Sergei <radchenkovsa@yandex.ru>
2023-09-27 00:41:51 +0300

    Буткемп. Програмирование. 3 урок.

---
+++ case1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Завести массив из 5 чисел и вывести суммму чисел массива
+
+int n = 5;
+int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int summa = 0;
+for (int i = 0; i < n; i++)
+    summa += array[i];
+Console.WriteLine(summa);
---
+++ case2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести таблицу умножения
+
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
